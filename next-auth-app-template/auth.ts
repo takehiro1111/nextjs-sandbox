6184d79fe4d62d9393a04cf4520018dc96fe0411
takehiro1111 <t.soccer.0916@gmail.com>
2026-01-11 17:14:05 +0900

    add as regular directory

---
+++ next-auth-app-template/auth.ts
@@ -0,0 +1,29 @@
+import NextAuth, { NextAuthConfig } from "next-auth";
+import GitHub from "@auth/core/providers/github"
+
+export const config: NextAuthConfig = {
+  providers: [
+    GitHub({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    }),
+  ],
+  basePath: "/api/auth",
+  callbacks: {
+    authorized({ request, auth }) {
+      try {
+        const { pathname } = request.nextUrl;
+        if (pathname === "/protected-page") return !!auth;
+        return true;
+      } catch (e) {
+        console.error(e);
+      }
+    },
+    jwt({ token, trigger, session }) {
+      if (trigger === "update") token.name = session.user.name;
+      return token;
+    },
+  },
+};
+
+export const { handlers, signIn, signOut, auth } = NextAuth(config);
